9e15f796a2d8a3d12a90521cc34278a6e3da7711
Shweta Singh <shwetasr2505@gmail.com>
2025-09-02 19:34:20 +0530

    demo

---
+++ app.js
@@ -0,0 +1,31 @@
+const express=require('express')
+const app=express();
+const bodyParser=require('body-parser');
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+
+app.use(bodyParser.json());
+
+app.post('/getResponse',(req,res)=>{
+    console.log(req.body.question)
+    const genAI = new GoogleGenerativeAI('AIzaSyDNkDqSdKikehfT7ryyz_5_Jh18bBe8EKM');
+    const model = genAI.getGenerativeModel ({model: "gemini-1.5-flash"});
+
+    model.generateContent(req.body.question).then(result=>{
+        console.log(result.response.text());
+        const response = result.response.text();
+        res.status(200).json({
+            respinse:response
+        })
+    
+    })
+    .catch(err=>{
+        console.log(err);
+        res.status(500).json({
+            error:err
+        })    
+    })
+})
+    
+
+module.exports=app;
