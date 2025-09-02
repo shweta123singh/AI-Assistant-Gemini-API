e5bc1e088b7202a38912bb6f6c0b0ef49127c315
Shweta Singh <shwetasr2505@gmail.com>
2025-09-02 19:48:00 +0530

    demo

--- app.js
+++ app.js
@@ -26,6 +26,12 @@ app.post('/getResponse',(req,res)=>{
         })    
     })
 })
+
+app.get('*',(req,res)=>{
+    res.status(404).json({
+        msg:'bad request'
+    })
+})
     
 
 module.exports=app;
