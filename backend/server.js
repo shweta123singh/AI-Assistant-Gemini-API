5f377b517bfa674264590641da40033ab5ce05bc
sh12 <147317222+shweta123singh@users.noreply.github.com>
2025-09-14 00:43:21 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
+// server.js
 const http = require('http');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000; // ✅ Fix bitwise OR to logical OR
 const app = require('./app');
 
 const server = http.createServer(app);
+
 server.listen(port, () => {
   console.log('App is running on port', port);
 });
+
 
 
